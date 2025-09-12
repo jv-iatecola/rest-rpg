24c912d87861bf59ebda3974fabab5daef7c13d5
jv-iatecola <jv.iatecola10@gmail.com>
2025-09-11 21:40:58 -0300

    chore: add repository

---
+++ sql/functions/get_character_id.sql
@@ -0,0 +1,25 @@
+-- ========================================
+-- Author:      jv-iatecola
+-- Created:     2025-09-10
+-- ========================================
+-- Description:
+-- - This file assumes an active connection to the target database.
+-- - Modify with caution; production impact possible.
+-- ========================================
+
+CREATE OR REPLACE FUNCTION get_character_id (p_name VARCHAR)
+RETURNS INT
+LANGUAGE sql
+AS $$
+DECLARE
+    v_id INT;
+BEGIN
+    SELECT id
+    INTO v_id
+    FROM characters
+    WHERE name = p_name
+
+    RETURN v_id;
+END;
+$$;
+
